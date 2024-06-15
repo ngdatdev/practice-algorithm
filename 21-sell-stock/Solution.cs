77e986bb732d0d785d87541eedea50099e8e1e65
ngdatdev <nvdatdz8b@gmail.com>
2024-06-15 19:20:37 +0700

    15/6/2024

---
+++ 21-sell-stock/Solution.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _21_sell_stock
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var result = 0;
+            var lowest = prices[0];
+
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] > prices[i + 1] && lowest > prices[i + 1])
+                {
+                    lowest = prices[i + 1];
+                }
+                var temp = prices[i + 1] - lowest;
+                if (prices[i] < prices[i + 1] && result < temp)
+                {
+                    result = temp;
+                }
+            }
+            return result;
+        }
+    }
+}
