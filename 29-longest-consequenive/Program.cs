31a8d0871446455bf4aa5fe7f6f20f7c0d8b8cf1
ngdatdev <nvdatdz8b@gmail.com>
2024-09-11 15:38:56 +0700

    11/9/2024

---
+++ 29-longest-consequenive/Program.cs
@@ -0,0 +1,5 @@
+﻿using _29_longest_consequenive;
+
+var s = new Solution();
+
+System.Console.WriteLine(s.LongestConsecutive([0,3,7,2,5,8,4,6,0,1]));
---
+++ 29-longest-consequenive/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _29_longest_consequenive
+{
+    public class Solution
+    {//[100,4,200,1,3,2]
+        public int LongestConsecutive(int[] nums) {
+            var hash = new HashSet<int>();
+            var temp = 0;
+
+            foreach(var item in nums) {
+                hash.Add(item);
+            }   
+
+            for(int i = 0; i < hash.Count(); i++) {
+                if(hash.Contains(nums[i] + 1)) {
+                    temp++;
+                }
+            }
+            return temp + 1;
+
+         }
+    }
+}
